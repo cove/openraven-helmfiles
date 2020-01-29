a5dcbd02f8da067e04af1d7ed7d1b82f6aeeda07
Matthew L Daniel <mdaniel@openraven.com>
2020-01-28 20:44:39 -0800

    Initial CI script for version bumping

---
+++ .gitlab/bump_chart_version.sh
@@ -0,0 +1,57 @@
+#! /usr/bin/env bash
+set -euo pipefail
+CI="${CI:-}"
+TRACE="${TRACE:-}"
+trace_on() {
+    [[ -n "$TRACE" ]] && set -x
+}
+trace_on
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $0 the-helmfile-filename the-charts-dirname the-new-version" >&2
+    exit 1
+fi
+# we could also take advantage of the fact that currently
+# the helmfile.d and chart names basically match
+helmfile_name="$1"
+shift
+chart_name="$1"
+shift
+chart_ver="$1"
+shift
+chart_yaml_fn=charts/${chart_name}/Chart.yaml
+helmfile_fn=helmfile.d/$helmfile_name
+
+if [[ -z "$CI" ]]; then
+    chat_start() {
+        :
+    }
+    chat_stop() {
+        :
+    }
+fi
+
+if [[ ! -e "$chart_yaml_fn" ]]; then
+    chat_start; echo "Sorry, your chart file \"$chart_yaml_fn\" is 404" >&2; chat_stop
+    exit 1
+fi
+if [[ ! -e "$helmfile_fn" ]]; then
+    chat_start; echo "Sorry, your helmfile.d file \"$helmfile_fn\" is 404" >&2; chat_stop
+    exit 1
+fi
+
+# watch out, this won't work for more complex setups like 13-aws-discovery-svc.yaml
+sed -i.bak -e "s/^  version: .*/  version: $chart_ver/" $helmfile_fn
+sed -i.bak -e "s/^version: .*/version: $chart_ver/" $chart_yaml_fn
+
+git add $helmfile_fn $chart_yaml_fn
+
+if [[ -n "$CI" ]]; then
+    set +x
+    git remote set-url --push origin \
+        $(echo $CI_REPOSITORY_URL | sed -e "s/${CI_REGISTRY_USER}[^@]*@/oauth2:${GLR_PAT}@/")
+    trace_on
+    git commit -m"Bump $chart_name to $chart_ver"
+    # skip CI jobs to (a) avoid any CI cycles (b) avoid any accidental rollouts
+    # maybe make this a job parameter?
+    git push -o ci.skip=true origin $CI_COMMIT_REF_NAME
+fi

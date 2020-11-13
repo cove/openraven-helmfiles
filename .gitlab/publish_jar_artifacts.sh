72d8705293e7307ab9e685f062e9ea5f8da9f712
Oliver Ferrigni <oliver@openraven.com>
2020-11-13 13:24:28 -0800

    Ease overriding of all_regions

--- .gitlab/publish_jar_artifacts.sh
+++ .gitlab/publish_jar_artifacts.sh
@@ -12,13 +12,12 @@ DMAP_ART_ID=dmap-lambda
 AWS_S3_ART_ID=aws-s3-scanner
 
 if [[ -z "${all_regions:-}" ]]; then
-  all_regions=(us-west-2 us-west-1 us-east-2 us-east-1 ca-central-1 sa-east-1 eu-west-3 eu-west-2 eu-west-1 eu-north-1 eu-central-1 ap-southeast-2 ap-southeast-1 ap-south-1 ap-northeast-2 ap-northeast-1)
+  all_regions=us-west-2 us-west-1 us-east-2 us-east-1 ca-central-1 sa-east-1 eu-west-3 eu-west-2 eu-west-1 eu-north-1 eu-central-1 ap-southeast-2 ap-southeast-1 ap-south-1 ap-northeast-2 ap-northeast-1
 fi
 
 copy_to_all_regions() {
   local fn="$1"
-  for region in "${all_regions[@]}"; do
-    aws s3 cp $fn s3://${HELM_S3_BUCKET}-${region}/dmap/dataclassification/$fn
+  for region in $all_regions; do
   done
 }
 

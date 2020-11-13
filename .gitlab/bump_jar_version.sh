a6af4d66387f519c615adcd7eab5332ae4fc8dbb
Oliver Ferrigni <oliver@openraven.com>
2020-11-12 21:21:39 -0800

    Automate bumping jar versions

---
+++ .gitlab/bump_jar_version.sh
@@ -0,0 +1,74 @@
+#! /usr/bin/env bash
+set -euo pipefail
+CI="${CI:-}"
+CHATOPS="${CHAT_CHANNEL:-}"
+TRACE="${TRACE:-}"
+trace_on() {
+    if [[ -n "$TRACE" ]]; then
+      set -x
+    fi
+}
+trace_on
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $0 the-jar-name the-new-version" >&2
+    exit 1
+fi
+
+# The jar name currently either aws-s3-scanner or dmap-lambda
+jar_name="$1"
+shift
+# The jar version of the form 0.0.PIPELINEID
+jar_ver="$1"
+shift
+# The location of the version file for the current jar
+jar_version_fn=helmfile.d/values/${jar_name}-version.yaml
+
+if [[ -z "$CHATOPS" ]]; then
+    chat_start() {
+        :
+    }
+    chat_stop() {
+        :
+    }
+fi
+
+if [[ ! -e "$jar_version_fn" ]]; then
+    chat_start; echo "Sorry, your chart file \"$jar_version_fn\" is 404" >&2; chat_stop
+    exit 1
+fi
+
+if [[ -n "$CI" ]]; then
+  echo "Putting the working copy back onto $CI_COMMIT_REF_NAME (was in detached head)" >&2
+  git checkout --force "$CI_COMMIT_REF_NAME"
+  git reset --hard "$CI_COMMIT_SHA"
+fi
+
+exit_if_not_dirty() {
+  local fn="$1"
+  if ! git status --porc "$fn" | grep -- "$fn"; then
+    echo "Expected the change to dirty \"$fn\" but nope" >&2
+    exit 1
+  fi
+}
+
+sed -i.bak -e "s/^version: .*/version: $jar_ver/" "$jar_version_fn"
+exit_if_not_dirty "$jar_version_fn"
+
+git add "$jar_version_fn"
+
+git diff --cached || true
+
+if [[ -n "$CI" ]]; then
+    set +x
+    # shellcheck disable=SC2001
+    push_url="$(echo "$CI_REPOSITORY_URL" | sed -e "s/${CI_REGISTRY_USER}[^@]*@/oauth2:${GLR_PAT}@/")"
+    git config --local user.name  "$GITLAB_USER_NAME"
+    git config --local user.email "$GITLAB_USER_EMAIL"
+    git remote set-url --push origin "$push_url"
+    trace_on
+    git commit -m"Bump $jar_name to $jar_ver
+
+    By request of @${GITLAB_USER_LOGIN}
+    "
+    git push origin "$CI_COMMIT_REF_NAME"
+fi
---
+++ .gitlab/publish_jar_artifacts.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+set -euo pipefail
+TRACE="${TRACE:-}"
+[[ -n "$TRACE" ]] && set -x
+HELM_S3_BUCKET="${HELM_S3_BUCKET:-}"
+if [[ -z "$HELM_S3_BUCKET" ]]; then
+    echo 'Not without $HELM_S3_BUCKET' >&2
+    exit 1
+fi
+
+HELM_S3_BUCKET_HOST="${HELM_S3_BUCKET_HOST:-s3.amazonaws.com}"
+
+HELM_S3_PATH_BASE="s3://${HELM_S3_BUCKET}/dmap"
+
+#Extract the version from ./values/aws-s3-scanner-version.yaml and ./values/dmap-lambda-version.yaml
+
+DMAP_LAMBDA_VERSION="$(awk '{print $2}' ./helmfile.d/values/dmap-lambda-version.yaml)"
+AWS_S3_SCANNER_VERSION="$(awk  '{print $2}' ./helmfile.d/values/aws-s3-scanner-version.yaml)"
+DMAP_LAMBDA_FN=aws-s3-scanner-${DMAP_LAMBDA_VERSION}.jar
+AWS_S3_SCANNER_FN=aws-s3-scanner-${AWS_S3_SCANNER_VERSION}.jar
+
+mvn -U dependency:copy -DoutputDirectory=. -Dartifact=io.openraven:aws-s3-scanner:${AWS_S3_SCANNER_VERSION}
+mvn -U dependency:copy -DoutputDirectory=. -Dartifact=io.openraven:dmap-lambda:${DMAP_LAMBDA_VERSION}
+
+all_regions=(us-west-2 us-west-1 us-east-2 us-east-1 ca-central-1 sa-east-1 eu-west-3 eu-west-2 eu-west-1 eu-north-1 eu-central-1 ap-southeast-2 ap-southeast-1 ap-south-1 ap-northeast-2 ap-northeast-1)
+
+for region in "${all_regions[@]}";do
+  aws s3 cp $AWS_S3_SCANNER_FN s3://$HELM_S3_BUCKET-$region/dmap/dataclassification/$AWS_S3_SCANNER_FN;
+done;
+
+# Fetch the artifacts from mvn
+
+# Iterate all the regions, uploading to s3 the artifacts
